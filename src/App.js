246df5ec6161359487dc16761b2e68e161df0b76
MalikNiazKhan <64806399+MalikNiazKhan@users.noreply.github.com>
2020-06-02 13:49:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+    <h1>Bootcamp2020 Second Assignment done by Malik Niaz. i.e. React-Base-App</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
